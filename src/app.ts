094d0a80aa3f7a03fb1c2f5b9c3348655fed0e5e
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-07-05 12:58:45 +0600

    fix: correct app export and import statements in app.ts and server.ts

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { type Application, type Request, type Response } from 'express'
 import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
-export const app: Application = express();
+const app: Application = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.get("/", (req: Request, res: Response) => {
@@ -13,3 +13,4 @@ app.get("/", (req: Request, res: Response) => {
         environment: process.env.NODE_ENV || "development",
     });
 });
+export default app;
