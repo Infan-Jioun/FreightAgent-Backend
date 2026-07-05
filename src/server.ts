8733e1d56e58975af252ccef49b7c303a2203be2
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-07-05 15:53:55 +0600

    feat: update server to use envConfig for port and environment mode logging

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
+import { envConfig } from "./_config/env";
 import app from "./app";
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+app.listen(envConfig.PORT, () => {
+    console.log(`Server is running on port ${envConfig.PORT} in ${envConfig.NODE_ENV} mode`);
 })
