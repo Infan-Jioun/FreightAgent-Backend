143df67d321a48f4ac7305a98a10aabadecd0d8b
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-07-05 15:53:12 +0600

    feat: implement environment variable loading with error handling

--- src/_config/env.ts
+++ src/_config/env.ts
@@ -1,1 +1,36 @@
-import dotenv from "dotenv";
+import dotEnv from "dotenv";
+import AppError from "../app/errorHelper/AppError";
+import status from "http-status";
+dotEnv.config();
+interface EnvConfig {
+    NODE_ENV: string,
+    PORT: string,
+    BETTER_AUTH_SECRET: string
+    BETTER_AUTH_URL: string
+    DATABASE_URL: string
+}
+
+const loadVariabales = (): EnvConfig => {
+    const requirementVariables = [
+
+        "NODE_ENV",
+        "PORT",
+        "BETTER_AUTH_SECRET",
+        "BETTER_AUTH_URL",
+        "DATABASE_URL",
+    ]
+    requirementVariables.forEach((variable) => {
+        if (!process.env[variable]) {
+            throw new AppError(status.INTERNAL_SERVER_ERROR, `Environment Variable ${variable} is required but not set in the .env file`)
+        }
+    })
+    return {
+        NODE_ENV: process.env.NODE_ENV as string,
+        PORT: process.env.PORT as string,
+        BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET as string,
+        BETTER_AUTH_URL: process.env.BETTER_AUTH_URL as string,
+        DATABASE_URL: process.env.DATABASE_URL as string,
+    }
+}
+
+export const envConfig = loadVariabales()
