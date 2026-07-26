a50990c9b696413235afb87703aa7e95c295443f
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-07-26 17:45:57 +0600

    feat(auth): refactor token utility functions and improve cookie handling

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -2,6 +2,7 @@ import { JwtPayload, SignOptions } from "jsonwebtoken";
 import { JwtTokenUtils } from "./jwt";
 import { envConfig } from "../_config/env";
 import { Request, Response } from "express";
+import { cookieUtils } from "./cookie";
 
 const getAccessToken = (payload: JwtPayload) => {
     const accessToken = JwtTokenUtils.createToken(
@@ -11,7 +12,7 @@ const getAccessToken = (payload: JwtPayload) => {
     )
     return accessToken;
 }
-const refreshToken = (payload: JwtPayload) => {
+const getRefreshToken = (payload: JwtPayload) => {
     const refreshToken = JwtTokenUtils.createToken(
         payload, envConfig.REFRESH_TOKEN_SECRET, {
             expiresIn: envConfig.REFRESH_TOKEN_EXPIRES_IN
@@ -19,6 +20,40 @@ const refreshToken = (payload: JwtPayload) => {
     )
     return refreshToken;
 }
-const setAccessTokenCookie = (res: Response, req: Request) => {
-   
+const setAccessTokenCookie = (res: Response, req: Request, token: string) => {
+    cookieUtils.setCookie(res, "accessToken", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+        maxAge: 60 * 60 * 60 * 24 * 1000
+    })
+}
+const setRefreshTokenCookie = (res: Response, token: string) => {
+    // const maxAge = ms(envVars.REFRESH_TOKEN_EXPIRES_IN as StringValue);
+    cookieUtils.setCookie(res, "refreshToken", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+        path: '/',
+        // 7days
+        maxAge: 60 * 60 * 60 * 24 * 1000 * 7
+    })
+}
+const setBetterAuthSessionCookie = (res: Response, token: string) => {
+    // const maxAge = ms(envVars.REFRESH_TOKEN_EXPIRES_IN as StringValue);
+    cookieUtils.setCookie(res, "better-auth.session_token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+        path: '/',
+        // 1 days
+        maxAge: 60 * 60 * 60 * 24
+    })
+}
+export const tokenUtils = {
+    getAccessToken,
+    getRefreshToken,
+    setAccessTokenCookie,
+    setRefreshTokenCookie,
+    setBetterAuthSessionCookie
 }
