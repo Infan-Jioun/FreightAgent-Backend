4eaf9636caee8fd55c1c9d2d952d31a32784b4c9
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-07-26 10:50:56 +0600

    refactor(auth): remove unnecessary blank lines in auth.router.ts

--- src/module/auth/auth.router.ts
+++ src/module/auth/auth.router.ts
@@ -3,10 +3,6 @@ import { authController } from "./auth.controller";
 import { validateRequest } from "../../middleware/validateRequest";
 import { registerSchema } from "./auth.validations";
 import { authenticate, authorize } from "../../middleware/auth";
-
-
-
-
 const router = Router();
 
 // Public routes
