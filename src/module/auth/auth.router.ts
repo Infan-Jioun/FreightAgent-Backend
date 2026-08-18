ea3e8a9a02e7ba4623c9c25349b4522de82cb421
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-08-18 14:01:24 +0600

    setup grapql

--- src/module/auth/auth.router.ts
+++ src/module/auth/auth.router.ts
@@ -3,7 +3,7 @@ import { authController } from "./auth.controller";
 import { validateRequest } from "../../middleware/validateRequest";
 import { loginSchema, registerSchema } from "./auth.validations";
 import { authenticate, authorize } from "../../middleware/auth";
-import { Role } from "../../generated/prisma";
+
 const router = Router();
 
 // Public routes
