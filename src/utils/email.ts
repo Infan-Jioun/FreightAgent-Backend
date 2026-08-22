fc3a5fa7866311842b1785db8d1ab59aaad4b780
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-08-22 12:18:03 +0600

    feat :create iterface SendEmailOption

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -1,7 +1,26 @@
 import nodemailer from "nodemailer"
+import { envConfig } from "../_config/env"
 const transporter = nodemailer.createTransport({
-    host : 
+    host: envConfig.EMAIL_HOST,
+    secure: false,
+    auth: {
+        user: envConfig.EMAIL_SMTP_USER,
+        pass: envConfig.EMAIL_SMTP_PASS
+    },
+    port: Number(envConfig.EMAIL_PORT)
 })
+// *File sneding
+interface SendEmailOption {
+    to: string;
+    subject: string;
+    templateName: string;
+    templateData: Record<string, any>;
+    attachments?: {
+        fileName: string,
+        content: string,
+        contentType: string
+    }[]
+}
 export const sendEmail = () => {
 
 }
